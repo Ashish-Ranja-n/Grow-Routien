bd121b153a91d7c523de44a27bd7189418bd4955
Ashish Ranjan <75297897+Ashish-Ranja-n@users.noreply.github.com>
2021-08-04 22:38:44 +0530

    Update index_javascript.js

--- index_javascript.js
+++ index_javascript.js
@@ -1,11 +1,21 @@
   var d = new Date().getDay()
- if (d == 1) {
-             document.getElementById("r3").classList.add("day");
+  if (d == 1) {
+             document.getElementById("r1").classList.add("day");
                 }
   eles if (d == 2) { 
-       document.getElementById("r3").classList.add("day");
+       document.getElementById("r2").classList.add("day");
          }
   eles if (d == 3) {
                document.getElementById("r3").classList.add("day");
                    }
+  eles if (d == 4) {
+               document.getElementById("r4").classList.add("day");
+                   }
+  eles if (d == 5) {
+               document.getElementById("r5").classList.add("day");
+                   }
+  eles if (d == 6) {
+               document.getElementById("r6").classList.add("day");
+                   }
+ 
          
